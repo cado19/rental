2deaf3e5cb12294355a1b61ad34ddbf0d6885147
cado19 <mryoungrichie@gmail.com>
2024-06-26 15:43:01 +0300

    added dummy sidebar content and css

--- home.php
+++ home.php
@@ -2,3 +2,59 @@
     // FOR NOW THIS WILL BE THE HOME DASHBOARD. wE'LL CUSTOMIZE IT AS THE APP GROWS
 
 ?>
+
+<div class="container">
+    <!-- Sidebar Section  -->
+     <aside>
+        <div class="toggle">
+            <div class="logo">
+                <img src="images/logo.png">
+                <h2>Rental<span class="danger">Magari</span></h2>
+            </div>
+
+            <div class="close" id="close-btn">
+                <span class="material-symbols-outlined">close</span>
+            </div>
+        </div>
+
+        <div class="sidebar">
+            <a href="#">
+                <span class="material-symbols-outlined">
+                    dashboard
+                </span>
+                <h3>Dashboard</h3>
+            </a>
+            <a href="#">
+                <span class="material-symbols-outlined">
+                    group
+                </span>
+                <h3>Users</h3>
+            </a>
+            <a href="#">
+                <span class="material-symbols-outlined">
+                    insights
+                </span>
+                <h3>Analytics</h3>
+            </a>
+            <a href="#">
+                <span class="material-symbols-outlined">
+                    menu_book
+                </span>
+                <h3>Bookings</h3>
+                <span class="message-count">27</span>
+            </a>
+            <a href="#">
+                <span class="material-symbols-outlined">
+                    inventory
+                </span>
+                <h3>Sale List</h3>
+            </a>
+        </div>
+     </aside>
+
+
+     <!-- Main Content  -->
+      <main>
+        
+      </main>
+</div>

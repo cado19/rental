b2e0b7139c1a6f12e6409d33b19187e011fd3ac7
cado19 <mryoungrichie@gmail.com>
2024-08-19 09:36:59 +0300

    some contract display changes

--- SQL/display_vehicles.sql
+++ SQL/display_vehicles.sql
@@ -1,1 +1,5 @@
-select vb.make AS Make, vb.model AS Model, vb.number_plate AS REG, vb.category AS Category, vp.daily_rate AS Rate FROM `kisuzi-rental`.`vehicle_basics` vb INNER JOIN `kisuzi-rental`.`vehicle_pricing` vp ON vb.id = vp.vehicle_id;   
+select vb.make AS Make, vb.model AS Model, vb.number_plate AS REG, vb.category AS Category, vp.daily_rate AS Rate FROM
+`kisuzi-rental`.`vehicle_basics` vb INNER JOIN `kisuzi-rental`.`vehicle_pricing` vp ON vb.id = vp.vehicle_id;   
+
+ALTER TABLE `kisuzi-rental`.`vehicle_basics` 
+ADD COLUMN `availability` VARCHAR(45) NULL AFTER `deleted`;

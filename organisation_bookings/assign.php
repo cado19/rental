91c0b064ca2d78cbfd22e120b2b5bd7c5fd06cdd
cado19 <mryoungrichie@gmail.com>
2024-12-07 11:09:40 +0300

    activate booking and cancel booking

---
+++ organisation_bookings/assign.php
@@ -0,0 +1,32 @@
+<?php
+$fuel = $result = $response = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$id = $_POST['id']; // this is the id of the booking
+	$account_id = $_POST['account_id'];
+	if (!empty($_POST['fuel'])) {
+		$fuel = $_POST['fuel'];
+	}
+	$result = assign_organisation_vehicle($id, $fuel, $account_id);
+
+	if ($result == "Success") {
+		//activate booking
+		$response = activate_organisation_booking($id);
+		if ($response == "Success") {
+			$msg = "Successfully assigned vehicle. Booking activated";
+			header("Location: index.php?page=organisation_bookings/show&id=$id&msg=$msg");
+		} else {
+			$err_msg = "There was an error activating the booking. Contact admin";
+			header("Location: index.php?page=organisation_bookings/show&id=$id&err_msg=$err_msg");
+		}
+	} else {
+		$err_msg = "There was an error assigning vehicle. Try again";
+		header("Location: index.php?page=organisation_bookings/show&id=$id&err_msg=$err_msg");
+	}
+
+} else {
+	$msg = "Unauthorized activity";
+	session_start();
+	session_destroy();
+	header("Location: index.php?msg=$msg");
+	exit;
+}

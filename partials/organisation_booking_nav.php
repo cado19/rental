d5d453e182107da541ec144210518238fdf406b8
cado19 <mryoungrichie@gmail.com>
2024-12-16 14:53:00 +0300

    organisation bookings nav

---
+++ partials/organisation_booking_nav.php
@@ -0,0 +1,8 @@
+    <!-- Account page navigation-->
+    <nav class="nav nav-borders">
+        <a class="nav-link active ms-0" href="index.php?page=organisation_bookings/all" target="__blank">General Bookings</a>
+        <a class="nav-link" href="index.php?page=organisation_bookings/upcoming" target="__blank">Upcoming Bookings</a>
+        <a class="nav-link" href="index.php?page=organisation_bookings/active" target="__blank">Active Bookings</a>
+        <a class="nav-link" href="index.php?page=organisation_bookings/completed"  target="__blank">Completed Bookings</a>
+        <a class="nav-link" href="index.php?page=organisation_bookings/cancelled"  target="__blank">Cancelled Bookings</a>
+    </nav>

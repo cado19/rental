d9adbb8e68c9af56cfd76820f43bc1ce5f62a72e
cado19 <mryoungrichie@gmail.com>
2025-01-24 12:28:21 +0300

    compress image function

--- config/functions/display_functions.php
+++ config/functions/display_functions.php
@@ -309,3 +309,32 @@ function show_monetary_value($object, $value)
 
         return $countries;
 }
+
+function compressImage($source, $destination, $quality) { 
+    // Get image info 
+    $imgInfo = getimagesize($source); 
+    $mime = $imgInfo['mime']; 
+     
+    // Create a new image from file 
+    switch($mime){ 
+        case 'image/jpeg': 
+            $image = imagecreatefromjpeg($source); 
+            imagejpeg($image, $destination, $quality);
+            break; 
+        case 'image/png': 
+            $image = imagecreatefrompng($source); 
+            imagepng($image, $destination, $quality);
+            break; 
+        case 'image/gif': 
+            $image = imagecreatefromgif($source); 
+            imagegif($image, $destination, $quality);
+            break; 
+        default: 
+            $image = imagecreatefromjpeg($source); 
+           imagejpeg($image, $destination, $quality);
+    } 
+     
+     
+    // Return compressed image 
+    return $destination; 
+} 
